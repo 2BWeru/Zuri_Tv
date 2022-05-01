7e8b4a659e676dd2becfc7fedef3c1ae33236084
Wamaitha <wamaithaweru19@gmail.com>
2022-05-01 19:48:18 +0300

    API Request

--- news/__init__.py
+++ news/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from .config import DevConfig
 
 news = Flask(__name__)   ##Initializing application
+
+# Setting up configuration
+news.config.from_object(DevConfig)
 
 from news import views
--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from news import news
 
+
 # views
 @news.route('/')
 def index():
@@ -8,4 +9,5 @@ def index():
      View root page function that returns the index page and its data
      '''
      message = 'Welcome to Zuri_Tv'
+     title = 'Home - Welcome to The best Movie Review Website Online'
      return render_template('index.html', message = message)
